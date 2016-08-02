9336d4913bb534e7460d49eeb3a4a3dc1235d9f4
Cumpanasu Florin <start_florin@yahoo.com>
2016-08-02 01:09:27 +0100

    Initial commit generated froma template in VisualStudio

---
+++ ExpressApp1/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/debug/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/inherits/index.d.ts" />
+/// <reference path="globals/jade/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/stylus/index.d.ts" />
